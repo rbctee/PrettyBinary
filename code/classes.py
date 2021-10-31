1757b74e2449bdf6d65d56d59d8c71e2d0add1aa
Robert <robert.raducioiu@whetu.com>
2021-10-31 17:07:28 -0400

    Convert little-endian bytes to hex

--- code/classes.py
+++ code/classes.py
@@ -1,4 +1,5 @@
 import sys
+import struct
 
 # currently the classes are written for x64 binaries
 class ExecutableHeader():
@@ -10,21 +11,21 @@ class ExecutableHeader():
             'ei_version': data[6:7].hex(),
             'ei_osabi': data[7:8].hex(),
             'ei_abiversion': data[8:9].hex(),
-            'ei_pad': data[9:16].hex()
+            'ei_pad': data[9:16][::-1].hex()
         }
-        self.e_type = data[16:18].hex()
-        self.e_machine = data[18:20].hex()
-        self.e_version = data[20:24].hex()
-        self.e_entry = data[24:32].hex()
-        self.e_phoff = data[32:40].hex()
-        self.e_shoff = data[40:48].hex()
-        self.e_flags = data[48:52].hex()
-        self.e_ehsize = data[52:54].hex()
-        self.e_phentsize = data[54:56].hex()
-        self.e_phnum = data[56:58].hex()
-        self.e_shentsize = data[58:60].hex()
-        self.e_shnum = data[60:62].hex()
-        self.e_shstrndx = data[62:64].hex()
+        self.e_type = data[16:18][::-1].hex()
+        self.e_machine = data[18:20][::-1].hex()
+        self.e_version = data[20:24][::-1].hex()
+        self.e_entry = data[24:32][::-1].hex()
+        self.e_phoff = data[32:40][::-1].hex()
+        self.e_shoff = data[40:48][::-1].hex()
+        self.e_flags = data[48:52][::-1].hex()
+        self.e_ehsize = data[52:54][::-1].hex()
+        self.e_phentsize = data[54:56][::-1].hex()
+        self.e_phnum = data[56:58][::-1].hex()
+        self.e_shentsize = data[58:60][::-1].hex()
+        self.e_shnum = data[60:62][::-1].hex()
+        self.e_shstrndx = data[62:64][::-1].hex()
 
 class ELF():
     def __init__(self, file_path):
